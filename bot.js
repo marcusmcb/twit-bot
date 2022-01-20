d878e05db6c453ac64ad78e026c1e331deb0c63d
Marcus McBride <marcusmcb@gmail.com>
2022-01-19 16:19:05 -0800

    added tweetIt, interval for testing, working

--- bot.js
+++ bot.js
@@ -12,24 +12,30 @@ const T = new Twit({
 })
 
 const composeTweet = (twitterContent) => {
-  // generates a random index value to select from array  
+  // generates a random index value to select from array
   const randomIndex = Math.floor(Math.random() * twitterContent.length)
   const item = twitterContent[randomIndex]
   let tweet = {
-    status: `${item.title} (${item.year}) ${item.url}`
-  }  
+    status: `${item.title} (${item.year}) ${item.url}`,
+  }
   return tweet
 }
 
-let newTweet = composeTweet(twitterContent)
+const tweetIt = (twitterContent) => {
+  let newTweet = composeTweet(twitterContent)
 
-const tweeted = (err, data, response) => {
-  if (err) {
-    console.log('ERROR: ', err)
-  } else {
-    console.log('SUCCESS: ', response)
+  let tweeted = (data, err, response) => {
+    if (err) {
+      console.log('ERROR: ', err)
+    } else {
+      console.log('SUCCESS: ', newTweet)
+    }
   }
+  // T.post('statuses/update', newTweet, tweeted)
+  console.log(newTweet)
 }
 
-// set interval on this method to automate new tweets
-T.post('statuses/update', newTweet, tweeted)
+tweetIt(twitterContent)
+setInterval(() => {
+  tweetIt(twitterContent)
+}, 1000 * 20)
