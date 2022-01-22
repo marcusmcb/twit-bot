f14e4cc767f52aed251adde3e9ea5611952eb639
Marcus McBride <marcusmcb@gmail.com>
2022-01-21 22:59:33 -0800

    added future dev notes, bot-backup & data structuring

--- bot.js
+++ bot.js
@@ -45,3 +45,36 @@ setInterval(() => {
 
 // functionality to post content relative to specific
 // dates, holidays, events, etc via tags in the data array.
+
+// FUTURE DEV
+
+// "bot backup"
+
+// we'll need to persist the list of indicies that have already
+// been selected/posted to avoid the twitter duplicate-post issue
+
+// we're passing our data.js to the bot and removing each random
+// index from twitterContent as they're selected
+// none of this data persists however and would be lost
+// anytime the script is interrupted
+
+// keep data.js as a "master list" to start but eventually migrate
+// json over to a proper mongo collection via mongoose
+
+// save array of prior random index selections
+// reference against current random index (helper method)
+// * if same, rerun random index generator and helper method
+// * if different, set random index and compose tweet
+
+// "data expansion"
+
+// add "original" property to each entry as an object with
+// title/year/url properties
+
+// when a random index is selected that has an "original" key
+// available, compose second tweet with the original's values
+// as a reply to the first tweet
+
+// check to see if there's an id property in the "tweeted"
+// callback response && can it be passed to the same
+// statuses/update endpoint as a param for an automated reply?
